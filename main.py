7b0435d40c5a899499328b2c7fad3159363cb013
Amith Kulkarni <103172775+amithuu@users.noreply.github.com>
2023-07-31 17:04:04 +0530

    This is a new commit for what I originally planned to be amended

---
+++ main.py
@@ -0,0 +1,84 @@
+# Todo: Need to make this Page scroll until we have as many adds, tomorrow[30/07/2023] need to add scroll bar to the self.root
+
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+from sign_up import Sign_up
+
+
+class TalentPlace:
+
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("1920x1080+0+0")
+        self.root.title("TalentPlace.ai")
+
+        img = Image.open(
+            r"C:\Users\Amith\Desktop\New folder\Photo\bgimage.png")
+        img = img.resize((1550, 500), Image.Resampling.LANCZOS)
+        self.image = ImageTk.PhotoImage(img)
+
+        f_lbl = Label(self.root, image=self.image)
+        f_lbl.place(x=0, y=0, width=1550, height=500)
+
+        img1 = Image.open(
+            r"C:\Users\Amith\Desktop\New folder\Photo\belowbg.jpg")
+        img1 = img1.resize((500, 250), Image.Resampling.LANCZOS)
+        self.image1 = ImageTk.PhotoImage(img1)
+
+        # company name link :todo: link need to make it.
+        company_name = Label(self.root, text="Talentplace.ai", font=(
+            "times new roman", 15, "bold"), background="#6007cb", foreground="white")
+        company_name.place(x=10, y=5, width=120, height=30)
+
+        # headings
+        title_heading = Label(f_lbl, text="Intelligent Resume To Skyrocket Your Career", font=(
+            "times new roman", 25, "bold"), background="#6007cb", foreground="White")
+        title_heading.place(x=450, y=100, width=700, height=50)
+
+        para = Label(f_lbl, text="For every working professional doing any kind of work", font=(
+            "times new roman", 18, "bold"), background="#6007cb", foreground="white")
+        para.place(x=490, y=160, width=600, height=30)
+
+        # Button get started
+        b1 = Button(self.root, text="Get Started", font=(
+            "times new roman", 20, "bold"), foreground="#6007cb", background="White", cursor="hand2", command=self.Sign_Up)
+        b1.place(x=700, y=250, width=180, height=35)
+
+        # down page
+        s_lbl = Label(self.root, image=self.image1)
+        s_lbl.place(x=950, y=530, width=500, height=250)
+
+        para1 = Label(self.root, text="Download as a", font=(
+            "times new roman", 35, "bold"), foreground="black", anchor="w")
+        para1.place(x=170, y=530, width=300, height=40)
+
+        para1 = Label(self.root, text="resume or share the", font=(
+            "times new roman", 35, "bold"), foreground="black", anchor="w")
+        para1.place(x=165, y=575, width=400, height=40)
+
+        para1 = Label(self.root, text="link online for quick", font=(
+            "times new roman", 35, "bold"), foreground="black", anchor="w")
+        para1.place(x=160, y=620, width=460, height=45)
+
+        para1 = Label(self.root, text="access and more", font=(
+            "times new roman", 35, "bold"), foreground="black", anchor="w")
+        para1.place(x=165, y=665, width=400, height=40)
+
+        para1 = Label(self.root, text="engagement", font=(
+            "times new roman", 35, "bold"), foreground="black", anchor="w")
+        para1.place(x=165, y=705, width=400, height=55)
+
+    "============Functions============"
+
+    def Sign_Up(self):
+        self.signup_window = Toplevel(self.root)
+        self.signup = Sign_up(self.signup_window)
+
+
+if __name__ == "__main__":
+
+    root = Tk()
+    "-> creating an object of root for Tk()"
+    obj = TalentPlace(root)
+    root = mainloop()
---
+++ sign_up.py
@@ -0,0 +1,85 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+
+class Sign_up:
+
+
+    def __init__(self, signup_page):
+        self.signup_page = signup_page
+        self.signup_page.geometry("1920x1080+0+0")
+        self.signup_page.title("Sign_Up")
+
+        # Side image
+
+        side_image = Image.open( r"C:\Users\Amith\Desktop\New folder\Photo\img2.jpg")
+        side_image = side_image.resize((700,1080), Image.Resampling.LANCZOS)
+        self.side_image = ImageTk.PhotoImage(side_image)
+
+        side_img = Label(self.signup_page, image = self.side_image)
+        side_img.place(x=0, y=0, width=700, height=1080)
+        
+        # log-in:
+        login_button = Button(self.signup_page, text="Log-in", font=("times new roman", 15,"bold", "underline"), foreground="#6007cb", cursor="hand2", relief="flat")
+        login_button.place(x=1420, y=0, width=85, height=40)
+
+        # frame for Sing-Up
+
+        signup_frame = LabelFrame(self.signup_page,bd=1, relief="flat", text="Join our Community", font=("times new roman", 15,"bold" ,"underline"), background="white", foreground="#6007cb")
+        signup_frame.place(x=900, y=200, width=500, height=500)
+
+        # Location label/Combo box:
+        # location = Label(signup_frame, text="Location", font=("times new roman", 15), anchor="w")
+        # location.grid(row=0, column=0)
+
+        # loc_combobox = ttk.Combobox(signup_frame, font=("times new roman", 15), width=20)
+        # loc_combobox["values"] =("Enter Location", "Bangalore", "Delhi", "Kashmir", "Pune", "Belgaum")
+        # loc_combobox.current(0)
+        # loc_combobox.grid(row=0,column=3)
+
+        """As there are so many dis-advantages using GRID so i am using manually for all the field, x and y ratio thing."""
+
+        # INSIDE FRAME ALL THE DETAILS
+
+        firstname = Label(signup_frame, text="First Name", font=("times new roman", 15), anchor="w",background="white")
+        firstname.place(x=20, y=10, width=100, height=15)
+
+        firstname_textbox = Entry(signup_frame, font=("times new roman", 15),bd=2)
+        firstname_textbox.place(x=20, y=40, width=170, height=30)
+
+        lastname = Label(signup_frame, text="Last Name", font=("times new roman", 15), anchor="w",background="white")
+        lastname.place(x=300, y=10, width=100, height=15)
+
+        lastname_textbox = Entry(signup_frame, font=("times new roman", 15),bd=2)
+        lastname_textbox.place(x=300, y=40, width=170, height=30)
+
+        Email_id = Label(signup_frame, text="Email", font=("times new roman", 15), anchor="w",background="white")
+        Email_id.place(x=20, y=110, width=100, height=15)
+
+        email_id_textbox = Entry(signup_frame, font=("times new roman", 15),bd=2)
+        email_id_textbox.place(x=20, y=140, width=250, height=30)
+
+        location = Label(signup_frame, text="Location", font=("times new roman", 15), anchor="w",background="white")
+        location.place(x=20, y=210, width=100, height=15)
+
+        loc_combobox = ttk.Combobox(signup_frame, font=("times new roman", 15), width=20)
+        loc_combobox["values"] =("Enter Location", "Bangalore", "Delhi", "Kashmir", "Pune", "Belgaum")
+        loc_combobox.current(0)
+        loc_combobox.place(x=20,y=240, width=170, height=30)
+
+        Password = Label(signup_frame, text="Password", font=("times new roman", 15), anchor="w",background="white")
+        Password.place(x=20, y=310, width=100, height=15)
+
+        password_textbox = Entry(signup_frame, font=("times new roman", 15), bd=2)
+        password_textbox.place(x=20, y=335, width=250, height=30)
+
+        # SignUp:
+        signup_button = Button(signup_frame, text="Sign-Up", font=("times new roman", 15,"bold"), background="#6007cb", cursor="hand2", relief="flat", foreground="white")
+        signup_button.place(x=190, y=410, width=150, height=40)
+
+if __name__ == "__main__":
+    signup_page = Tk()
+    obj = Sign_up(signup_page)
+    signup_page = mainloop()
+
